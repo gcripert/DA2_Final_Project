ec1564e5e11461472198918fc34a644dc22cbaf9
gcripert <122497730+gcripert@users.noreply.github.com>
2023-07-27 01:04:57 -0400

    cleaning up annotations

--- Fashion_Analysis.py
+++ Fashion_Analysis.py
@@ -10,13 +10,13 @@ import pandas as pd
 
 def create_table_from_csv(sql, csv_file_path, table_name):
 
-    # Use 'reader' to read the headers; these will be the column names for the table
+    # Using 'reader' to read the headers; these will be the column names for the table
 
     with open(csv_file_path, 'r', newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         headers = next(csvreader)
 
-    # Determine the data types for each column based on the first row of data in the CSV with no empty values
+    # Determining the data types for each column based on the first row of data in the csv with no empty values
 
     with open(csv_file_path, 'r', newline='') as csvfile:
         csvreader = csv.DictReader(csvfile)
@@ -28,11 +28,14 @@ def create_table_from_csv(sql, csv_file_path, table_name):
     column_types = {}
     for header, value in sample_row.items():
         if value.isdigit():
-            # Check if the value is an integer
+
+            # Checking if the value is an integer
+
             column_types[header] = 'INTEGER'
         else:
             try:
-                # Check if the value is a float
+                # Checking if the value is a float
+
                 float_value = float(value)
                 if float_value.is_integer():
                     column_types[header] = 'INTEGER'
@@ -40,23 +43,28 @@ def create_table_from_csv(sql, csv_file_path, table_name):
                     column_types[header] = 'REAL'
             except ValueError:
                 try:
-                    # Check if the value is a date
+                    # Checking if the value is a date
+
                     datetime.strptime(value, '%Y-%m-%d')
                     column_types[header] = 'DATE'
                 except ValueError:
-                    # If none of the above conditions are met, consider it as text
+
+                    # If none of the above conditions are met, consider the value as text
+
                     column_types[header] = 'TEXT'
 
-    # Generate the SQL CREATE TABLE statement
+    # Generating the sql create table statement
+
     columns = ", ".join([f"{header} {column_types[header]}" for header in headers])
     create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
 
-    # Create the table
+    # Creating the table
+
     sql.execute(create_table_sql)
 
 
 
-# Identifying if the table is empty
+# Function for determining if the table is empty
 
 def table_is_empty(sql, table_name):
     sql.execute(f'SELECT COUNT(*) FROM {table_name}')
@@ -65,43 +73,44 @@ def table_is_empty(sql, table_name):
 
 
 
-# Importing data from csv into sqlite table
+# Function for importing data from csv into sqlite table
 
 
 def import_data_from_csv(csv_file_path, db_file_path, table_name):
 
-    # Connect to the SQLite database. uri=True will create the database if it does not already exist.
+    # Connecting to the SQLite database. uri=True will create the database if it does not already exist
 
     conn = sqlite3.connect(db_file_path, uri=True)
     sql = conn.cursor()
 
-    # Create the table (if it doesn't exist) based on the CSV data
+    # Creating the table (if it doesn't exist) based on the csv data
 
     create_table_from_csv(sql, csv_file_path, table_name)
 
-    # Check if the table is not empty
+    # Checking if the table is not empty
 
     if not table_is_empty(sql, table_name):
         print(f"The table '{table_name}' is not empty. Data import skipped.")
         conn.close()
         return
 
-    # Read data from the CSV file and insert into the SQLite table
+    # Reading data from the csv file and inserting it into the SQLite table
 
     with open(csv_file_path, 'r', newline='') as csvfile:
         csvreader = csv.reader(csvfile)
 
-        # Skip the header row
+        # Skipping the header row
 
         next(csvreader)
 
         for row in csvreader:
 
-            # Assuming the CSV file columns are in the same order as the table columns
+            # Creating sql INSERT for data row
 
             sql.execute(f'INSERT INTO {table_name} VALUES ({", ".join("?" * len(row))})', row)
 
-    # Commit the changes and close the connection
+    # Committing the changes and closing the connection
+
     conn.commit()
     conn.close()
 
@@ -109,43 +118,57 @@ def import_data_from_csv(csv_file_path, db_file_path, table_name):
 # Testing
 
 def print_first_five_rows(db_file_path, table_name):
-    # Connect to the SQLite database
+
+    # Connecting to the SQLite database
+
     conn = sqlite3.connect(db_file_path)
     sql = conn.cursor()
 
-    # Execute the SELECT query to retrieve the first five rows from the table
+    # Executing the SELECT query to retrieve the first five rows from the table
+
     sql.execute(f'SELECT * FROM {table_name} LIMIT 5')
     rows = sql.fetchall()
 
     if not rows:
         print(f"The table '{table_name}' is empty.")
     else:
-        # Print the first five rows
+
+        # Printing the first five rows
+
         print(f"First five rows of table '{table_name}':")
         for row in rows:
             print(row)
 
-    # Close the connection
+    # Closing the connection
+
     conn.close()
+
+# Function for querying the database and printing the results
 
 def query_and_print(database_file, query):
     try:
-        # Connect to the database
+        # Connecting to the database
+
         connection = sqlite3.connect(database_file)
         cursor = connection.cursor()
 
-        # Execute the query
+        # Executing the query
+
         cursor.execute(query)
 
-        # Fetch all the results
+        # Fetching all results
+
         results = cursor.fetchall()
 
         if len(results) > 0:
-            # Print the column headers
+
+            # Printing the column headers
+
             column_names = [description[0] for description in cursor.description]
             print('\t'.join(column_names))
 
-            # Print the results
+            # Printing the results
+
             for row in results:
                 print('\t'.join(str(value) for value in row))
         else:
@@ -155,47 +178,55 @@ def query_and_print(database_file, query):
         print("An error occurred:", e)
 
     finally:
-        # Close the connection
+
+        # Closing the connection
+
         if connection:
             connection.close()
 
-# Call the function to import data from the CSV to the SQLite database
+# Call the function to import data from the csv to the SQLite database
 
-""" 
+
 import_data_from_csv('Data/articles.csv', 'Data\\HM_data.db', 'articles')
 import_data_from_csv('Data/customers.csv', 'Data\\HM_data.db', 'customers')
 import_data_from_csv('Data/transactions_train.csv', 'Data\\HM_data.db', 'transactions')
+
+# Verifying the data was properly imported
 
 
 print_first_five_rows('Data\\HM_data.db', 'articles')
 print_first_five_rows('Data\\HM_data.db', 'customers')
 print_first_five_rows('Data\\HM_data.db', 'transactions') 
 
+# Checking the date range of the data
 
 query_and_print('Data\\HM_data.db', 'Select * from transactions order by t_dat asc limit 5')
 query_and_print('Data\\HM_data.db', 'Select * from transactions order by t_dat desc limit 5')
 
+# Checking the age range of the data
 
 query_and_print('Data\\HM_data.db', "Select min(age), max(age) from customers where age!=''")
 
+# Connecting to sqlite
+
 conn = sqlite3.connect('Data\\HM_data.db')
 sql = conn.cursor()
+
+# Creating indexes for age and price to speed up sqlite queries
+
 sql.execute('CREATE INDEX idx_age ON Customers (age)')
 sql.execute('CREATE INDEX idx_price ON Transactions (price)')
+
+# Removing records with empty ages and prices
 
 sql.execute("DELETE FROM customers WHERE age = ''")
 sql.execute("DELETE FROM transactions WHERE price = ''")
 
-# query_and_print('Data\\HM_data.db', query)
 
-"""
-
-# https://www.kaggle.com/competitions/h-and-m-personalized-fashion-recommendations/discussion/310496 -Origin of 590x
-
-
-conn = sqlite3.connect('Data\\HM_data.db')
-sql = conn.cursor()
-
+# Origin of 590x, which was used in the data as the multiplier of price to get dollar values. https://www.kaggle.com/competitions/h-and-m-personalized-fashion-recommendations/discussion/310496 
+# Joining transactions and customers to analyze customer spending
+# Grouping by age range, separated by decade using CASE
+# Using COUNT, ROUND, and SUM to calculate percentages and totals
 
 query="""
 SELECT CASE
@@ -212,13 +243,15 @@ SELECT CASE
 FROM Transactions
 INNER JOIN Customers ON Transactions.customer_id = Customers.customer_id
 GROUP BY age_group;
-
 """
 
+# Executing query and storing the results in a dataframe
 
 sql.execute(query)
 results = sql.fetchall()
 df = pd.DataFrame(results, columns=['age_group', 'num_customers', 'population_share_percentage', 'transaction_total', 'transaction_average'])
+
+# Saving results data to csv for use in a Tableau dashboard visualization
 
 df.to_csv('age_data.csv')
 
